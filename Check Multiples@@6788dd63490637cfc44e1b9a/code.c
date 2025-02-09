0d54de69f32ef17e0474380ef69b25d1756a6e57
Aniket Singla <aniket0260.becse24@chitkara.edu.in>
2025-02-09 23:00:41 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,divisor;
+    scanf("%d %d",&num,&divisor);
+    if(num % divisor==0)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
